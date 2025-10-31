8a45fb524fe974b83f1b70e0ec7680917f2b711f
Rooniii <itsroonikz@gmail.com>
2025-10-31 20:09:15 -0300

    Add files via upload

--- habprof/routes/web.php
+++ habprof/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\clogin\loginController;
+use App\Http\Controllers\cdashboard\dashboardController;
 
-Route::get('/', function () {
-    return view('mainMenu');
-});
+// Página principal (login)
+Route::get('/', [loginController::class, 'mostrarLogin'])->name('login.mostrar');
+
+// Validación del login
+Route::post('/login', [loginController::class, 'validarLogin'])->name('login.validar');
+
+Route::get('/dashboard', function () {
+    return view('dashboard.inicio');
+})->middleware('auth')->name('dashboard.inicio');
